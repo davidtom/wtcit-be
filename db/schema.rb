206d37ecc05d491d65df83e6f06942ea4d406666
github username <github email address>
2017-08-02 10:47:18 -0400

    comment model, associations, and controller created, waiting for API call

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801205238) do
+ActiveRecord::Schema.define(version: 20170802142259) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "user_id"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
