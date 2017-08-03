c8c5a71d1a766ebc5e13a863e6fec2382d09199b
davidtom <davidjtomczyk@gmail.com>
2017-08-03 16:08:34 -0400

    comments can now be made anonymously

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,9 @@ class CommentsController < ApplicationController
 
   def create
     game = Game.find(params[:game][:id])
-    user = User.find_or_create_by(name: params[:userName])
+    userName = anonymous_user?(params[:userName])
+    user = User.find_or_create_by(name: userName)
+    # user = User.find_or_create_by(name: params[:userName])
     comment = Comment.create(game_id: game.id, user_id: user.id, text: params[:commentText])
     user.comments << comment
     render json: { comment: comment, user: user, game: game }
