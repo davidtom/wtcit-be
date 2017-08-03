39945b303714b4d86f91a359bdcb1118856effba
davidtom <davidjtomczyk@gmail.com>
2017-08-03 16:02:20 -0400

    fix logic to assign anonymous username

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,7 +2,8 @@ class GuessesController < ApplicationController
 
   def create
     game = Game.find(params[:game][:id])
-    user = User.find_or_create_by(name: params[:userName])
+    userName = anonymous_user?(params[:userName])
+    user = User.find_or_create_by(name: userName)
     guess = Guess.create(game_id: game.id, user_id: user.id, text: guess_text)
     user.guesses << guess
     game.update(complete: guess.correct)
